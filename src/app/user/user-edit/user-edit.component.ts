4cd11afe326c3c0b7d81e8b66d07427ef2945fcb
Kellan Arick <kellan5015@gmail.com>
2019-03-16 16:44:01 -0400

    create page and home page

--- src/app/user/user-edit/user-edit.component.ts
+++ src/app/user/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'
+import { UserService } from '../user.service';
+import { User } from '../user.class';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User('', '', '', '', '', '');
+
+  save(): void{
+    this.usersvc.change(this.user)
+      .subscribe(
+        resp =>{ //sucess
+          console.log(resp)
+          this.router.navigateByUrl('/user/list')
+        },
+        err => { //error
+          console.log(err)
+        }
+      );
+    }
+  constructor(private usersvc: UserService, private router: Router) { }
 
   ngOnInit() {
   }
